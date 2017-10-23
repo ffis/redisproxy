cf8a442c912033aecabfb4ffbfa452c4474eafa2
loksly <loksly@gmail.com>
2017-10-23 17:35:40 +0200

    Added a CAS validation

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 This is a simple redis proxy intented to server a jsonp access.
 
-Note this service is passwordless and it's not intented to be deployed outside of the DMZ
+Note this service has a not to strong CAS validation.
+Check the code before using it on a production environment or at least don't deploy it outside of the DMZ
 nor having the redis any kind of confidencial information.
 
 ## Installation
@@ -16,4 +17,16 @@ npm install
 
 ``` 
 
-Then check config.json to configure redis connection parameters.
+Duplicate _config-example.json_ and rename to _config.json_ configure redis connection parameters.
+
+## Check Redis is working
+
+Suppose the Redis server is configured on host 127.0.0.1, then run this script:
+
+```bash
+$ wget https://github.com/crypt1d/redi.sh/raw/master/redi.sh
+$ echo "this is a variable" | bash redi.sh -s testvar -H 127.0.0.1
+$ bash redi.sh -g testvar -H 127.0.0.1
+# If you can read "this is a variable" then everything is ok.
+```
+
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-(function(){
+(function(logger){
 	'use strict';
 
 	const express = require('express'),
 		redis = require('redis'),
 		url = require('url'),
 		BloomFilter = require('bloom.js'),
+		CAS = require('cas'),
+		jsonwebtoken = require('jsonwebtoken'),
 		config = require('./config.json'),
-		app = express();
+		app = express(),
+		cas = new CAS(config.cas);
 
 	const clients = [];
 
@@ -20,16 +23,15 @@
 	});
 
 	function refreshbloom(cb){
-		redisclient.keys('*', function(err, vals){
+		return redisclient.keys('*', function(err, vals){
 			if (!err){
 				filter = new BloomFilter();
 				vals.forEach(function(value){
 					filter.add(value);
 				});
 			}
-			if (cb){
-				cb(err);
-			}
+
+			return cb ? cb(err) : true;
 		});
 	}
 
@@ -42,31 +44,48 @@
 		Reflect.deleteProperty(config.redis.password);
 	}
 
-	const existingkeys = {};
+	app.use('/authenticate', function(req, res){
+		if (typeof req.query.ticket === 'string' && req.query.ticket !== ''){
+			cas.validate(data.ticket, function(err, status, username){
+				if (err){
+					res.status(400);
+
+					return;
+				}
+				const token = jsonwebtoken.sign({username: username}, config.jwt.secret, {'expiresIn': config.jwt.session_time});
+
+				res.jsonp({token: token});
+			});
+		} else {
+			res.status(400);
+		}
+	});
 
 	app.get('*', function(req, res){
 		const key = url.parse(req.url).pathname;
-		if (typeof filter.contains(key)){
-			console.log('bloom ok', key);
+
+		if (filter.contains(key)){
 			redisclient.get(key, function(err, val){
 				if (err){
 					res.status(500);
-				} else if (!val){
-					res.status(404);
-				} else {
+				} else if (val){
+
 					res.jsonp(val);
+				} else {
+					res.status(404);
 				}
 			});
 		} else {
-			refreshbloom(function(err){
-				if (typeof filter.contains(key)){
+			refreshbloom(function(){
+				if (filter.contains(key)){
 					redisclient.get(key, function(erro, val){
 						if (erro){
 							res.status(500);
-						} else if (!val){
-							res.status(404);
-						} else {
+						} else if (val){
+
 							res.jsonp(val);
+						} else {
+							res.status(404);
 						}
 					});
 				} else {
@@ -77,47 +96,57 @@
 	});
 
 
+	let server = false;
+
 	if (config.server.https){
 		const https = require('https'),
 			fs = require('fs');
 		config.server.options.key = fs.readFileSync(config.server.options.key);
 		config.server.options.cert = fs.readFileSync(config.server.options.cert);
-		https.createServer(config.server.options, app).listen(config.server.port);
+		server = https.createServer(config.server.options, app);
 		Reflect.deleteProperty(config.server, 'options');
 
 	} else {
-
-		const server = require('http').createServer(app);
-		const io = require('socket.io')(server);
-		
-		io.on('connection', function(client){
-			clients.push(client);
-			console.log('Added client', clients.length)
-
-			client.on('event', function(data){
-				console.log('helllo', data);
-			});
-			client.on('disconnect', function(){
-				const index = clients.indexOf(client);
-				if (index > -1){
-					clients.splice(index, 1);
-				}
-				console.log('removed client', clients.length);
-			});
-		});
-
-		redissubs.subscribe('updates', function(evt){
-			console.log(evt);
-		});
-		redissubs.on('message', function (channel, data) {
-			console.log(channel, data);
-			clients.forEach(function(socket){
-				socket.send(data);
-			});
-		});
-
-
-		server.listen(config.server.port);
+		server = require('http').createServer(app);
 	}
+	const io = require('socket.io')(server);
+	
+	io.on('connection', function(client){
+		
+		logger.log('Added client', clients.length);
 
-})();
+		client.on('event', function(data){
+			logger.log(data);
+			cas.validate(data.ticket, function(err, status, username){
+				if (err){
+					logger.log(data, err);
+				} else {
+					logger.log(data, status, username);
+					clients.push(client);
+				}
+			});
+		});
+		client.on('disconnect', function(){
+			const index = clients.indexOf(client);
+			if (index > -1){
+				clients.splice(index, 1);
+			}
+			logger.log('removed client', clients.length);
+		});
+	});
+
+	redissubs.subscribe('updates', function(evt){
+		logger.log(evt);
+	});
+	redissubs.on('message', function (channel, data) {
+		logger.log(channel, data);
+		clients.forEach(function(socket){
+			socket.send(data);
+		});
+	});
+
+
+	server.listen(config.server.port);
+
+
+})(console);
